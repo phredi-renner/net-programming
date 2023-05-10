0f8954724572ca73ce24a6386cac04b132ee329a
phredi-renner <52304999+phredi-renner@users.noreply.github.com>
2023-05-10 15:40:01 -0700

    revised for B750

--- InterfaceDescription/chassis.py
+++ InterfaceDescription/chassis.py
@@ -8,6 +8,7 @@
 # Date: 3/10/2023
 # V1.1 - added comments and info to reference proper CV configlet
 # V1.2 - 4/25/23 - adding phone trunk untagged
+# V1.3 - 5/9/23 - modificaitons to input spreadsheet formatting
 ############################################################################
 
 import openpyxl
@@ -24,25 +25,29 @@ for sheet in wb.sheetnames:
     # define stuff we will work with
     page = wb[sheet]
     header = page['A1'].value
-    outputfile = 'output/'+header +'.ios'
-    idf = page['D2'].value
-    bldg = page['E2'].value
-    sw_ip = page['F2'].value
-    d_vlan = page['G2'].value
-    v_vlan = page['H2'].value
-    s_vlan = page['I2'].value
-    sp_vlan = page['J2'].value
+    sw_hostname = page['B3'].value
+   #outputfile = 'output/'+header +'.ios'
+    outputfile = 'output/'+ sw_hostname +'.ios'
+    idf = page['E2'].value
+    bldg = page['F2'].value
+    sw_ip = page['G2'].value
+    d_vlan = page['H2'].value
+    v_vlan = page['I2'].value
+    s_vlan = page['J2'].value
+    sp_vlan = page['K2'].value
     
     with open(outputfile, 'w') as output:
+    #    print(f"Page equals: {page}")
         # Create a header so we know what we are getting
 #        ! CV Filename: cus_{{vrf}}_vrf_wb-us-bur-b{{bldg}}f{{floor}}-as{{sw_num}}_{{mgt_ip}}
         output.write('!============================\n')
-        output.write('!CV filename:cus_interface_' + header + '_' + str(sw_ip) + '\n')
+        output.write('!CV filename:cus_interface_' + sw_hostname + '_' + str(sw_ip) + '\n')
         output.write('!\n')
         output.write('! Paste everything below in the configlet\n')
         output.write('!-------\n')
-        output.write('!CusConfig:interface_' + header + '_' + str(sw_ip) + '\n')
+        output.write('!CusConfig:interface_' + sw_hostname + '_' + str(sw_ip) + '\n')
         output.write('!============================\n')
+        output.write('!' + sw_hostname + '\n')
         output.write('!' + header + ' - ' + str(sheet) + '\n')
         output.write('!============================\n')
         output.write('\n')
@@ -51,7 +56,7 @@ for sheet in wb.sheetnames:
         for i in range(3, page.max_row+1, 1):
             # If it's a wireless AP we use a different port configuration
             if 'AP' in page.cell(row=i, column=1).value:
-                output.write(f'interface Ethernet' + str(page.cell(row=i, column=2).value) + '/' + str(page.cell(row=i, column=3).value) + '\n')
+                output.write(f'interface Ethernet' + str(page.cell(row=i, column=3).value) + '/' + str(page.cell(row=i, column=4).value) + '\n')
                 output.write(f' description '+ str(bldg) + '-' + str(idf) + '-' + str(page.cell(row=i, column=1).value) + '\n')
                 output.write(' switchport access vlan ' + str(d_vlan) + '\n')
                 output.write(' no poe disable\n')
@@ -59,14 +64,14 @@ for sheet in wb.sheetnames:
                 output.write('!\n')
             # if it's a camera then we have a different vlan
             elif 'CAM' in page.cell(row=i, column=1).value:
-                output.write(f'interface Ethernet' + str(page.cell(row=i, column=2).value) + '/' + str(page.cell(row=i, column=3).value) + '\n')
+                output.write(f'interface Ethernet' + str(page.cell(row=i, column=3).value) + '/' + str(page.cell(row=i, column=4).value) + '\n')
                 output.write(f' description '+ str(bldg) + '-' + str(idf) + '-' + str(page.cell(row=i, column=1).value) + '\n')
                 output.write(' switchport access vlan ' + str(s_vlan) + '\n')
                 output.write(' no poe disable\n')
                 output.write(' no shutdown\n')
                 output.write('!\n')
             elif 'SP' in page.cell(row=i, column=1).value:
-                output.write(f'interface Ethernet' + str(page.cell(row=i, column=2).value) + '/' + str(page.cell(row=i, column=3).value) + '\n')
+                output.write(f'interface Ethernet' + str(page.cell(row=i, column=3).value) + '/' + str(page.cell(row=i, column=4).value) + '\n')
                 output.write(f' description '+ str(bldg) + '-' + str(idf) + '-' + str(page.cell(row=i, column=1).value) + '\n')
                 output.write(' switchport access vlan ' + str(sp_vlan) + '\n')
                 output.write(' no poe disable\n')
@@ -74,7 +79,7 @@ for sheet in wb.sheetnames:
                 output.write('!\n')
             else:
                 # Default port configuration
-                output.write(f'interface Ethernet' + str(page.cell(row=i, column=2).value) + '/' + str(page.cell(row=i, column=3).value) + '\n')
+                output.write(f'interface Ethernet' + str(page.cell(row=i, column=3).value) + '/' + str(page.cell(row=i, column=4).value) + '\n')
                 output.write(f' description '+ str(bldg) + '-' + str(idf) + '-' + str(page.cell(row=i, column=1).value) + '\n')
             #    output.write(' switchport access vlan ' + str(d_vlan) + '\n')
                 output.write(' switchport trunk native vlan ' + str(d_vlan) + '\n')
